6399f03b94ad5978bc8309b0fcf0514ec2d82423
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2026-08-27 16:57:54 -0300

    Implementar método Index en InquilinoController y mejorar validaciones en el modelo Inquilino

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -1,10 +1,16 @@
 namespace Inmobiliaria.Models;
-
+using System.ComponentModel.DataAnnotations;
 public class Inquilino
 {
     public int Id_inquilino { get; set; }
+
+    [Required(ErrorMessage = "El DNI es obligatorio")]
+    [StringLength(8, ErrorMessage = "El DNI debe tener 8 caracteres")]
     public string Dni { get; set; } = ""; 
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
     public string Nombre { get; set; } = "";
+    [Required(ErrorMessage = "El apellido es obligatorio")]
     public string Apellido { get; set; } = "";
     public string Telefono { get; set; } = "";
     public string Email { get; set; } = "";
