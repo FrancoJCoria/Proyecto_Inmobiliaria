daf08fdc6b0e82fb06c4a3b66771d5e370b14339
Emi-legui <emilegui76@gmail.com>
2026-09-01 12:33:33 -0300

    Solucionando conflicto
    Se agrega required en line 8, 13,16

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -5,15 +5,15 @@ namespace Inmobiliaria.Models;
 public class Inmueble
 {
     public int Id_inmueble { get; set; }
-    public string direccion {get; set; }
+    public required string direccion {get; set; }
     public int cupo {get; set; }
     public decimal precio_dia {get; set; }
     public decimal porcentaje_reserva {get; set; }
     public bool disponible {get; set; }
-    public string portada {get; set; }
+    public required string portada {get; set; }
     public int id_propietario {get; set; }
     public int id_tipo {get; set; }
-    public string estado {get; set; }
+    public required string estado {get; set; }
      public override string ToString()
     {
         return $"(Id_inmueble: {Id_inmueble}), Direccion: {direccion}, Cupo: {cupo}, Precio por dia: {precio_dia}, Porcentaje de reserva: {porcentaje_reserva}, Disponible: {disponible}, Portada: {portada}, Id_propietario: {id_propietario}, Id_tipo: {id_tipo}, Estado: {estado}";
